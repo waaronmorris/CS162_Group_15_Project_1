e4360e78315d4d9fed16a00e6706d04f87c75c5a
troelze <34492927+troelze@users.noreply.github.com>
2018-07-25 13:33:30 -0700

    Just a place to start
    Simply makes the 2d board array (this can be accomplished in our Game class if we want to but I did it here to keep it simple for now. If you compile this with Critter.cpp on flip you get a print out of our board with 0s and 1s wherever **.placeCritter(x,y) has been called.

--- main.cpp
+++ main.cpp
@@ -4,4 +4,46 @@
 ** Date:
 ** Description:
 *********************************************************************/
+#include "Critter.hpp"
+#include <iostream>
+
+int main()
+{
+	//double pointer initiation
+	Critter** critterArray;
+	
+	//dynamically allocate array of 20 Critter-pointers
+	critterArray = new Critter*[20];
+	for (int i = 0; i < 20; i++)
+	{
+		//for each Critter-pointer, dynamically allocate another array of 20 Critters
+		critterArray[i] = new Critter[20];
+	}
+	
+	//print the current board of empty spaces (0s)
+	critterArray[0][0].printBoard();
+	
+	
+	
+	std::cout << std::endl;
+
+	//place critter changes the spot on board given by the x,y paramaters to "1"
+	//because it was called by a different critter object, i'm demonstrating how
+	//any of the critter objects in the critter 2d arraay can change the static board
+	//member
+	critterArray[1][0].placeCritter(3,4);
+	//then any of the critter objects will print the current, updated board
+	critterArray[2][4].printBoard();
+
+	
+	//free dynamic memory
+	for (int i = 0; i <20; i++)
+	{
+		delete [] critterArray[i];
+	}
+
+	delete [] critterArray;
+	
+	return 0;
+}
 
